f3260e1fe61d8ec5241fd60de8c1d5ae1ae659d8
hongsoom <hongsoom@kakao.com>
2022-08-29 16:24:55 +0900

    initial environment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
 
-function App() {}
+function App() {
+  return (
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/main" element={<Main />} />
+    </Routes>
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
-import theme from "./shared/theme";
-import ScrollToTop from "./components/ScrollToTop";
+import ScrollToTop from "./components/share/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <ScrollToTop />
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
   </Provider>
 );
