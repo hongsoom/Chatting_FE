440dce27346e13d0addd1685a91c6903cd6dcb3c
hongsoom <hongsoom@kakao.com>
2022-08-30 13:20:41 +0900

    Feat : feat 로그인, 회원가입 리덕스 큰틀 추가

---
+++ src/redux/modules/user.js
@@ -0,0 +1,33 @@
+import { createAction, handleActions } from "redux-actions";
+import { produce } from "immer";
+import instance from "../request";
+
+const SIGNUP = "signup";
+const LOGIN = "login";
+const LOGOUT = "logout";
+const IDCHECK = "idcheck";
+const NICKNAMECHECK = "nicknamecheck";
+const MYINFO = "myinfo";
+const USERINFO = "userinfo";
+const EDITMYINFO = "editinfo";
+
+const initialState = {
+  list: [],
+  isLogin: false,
+  message: "",
+};
+
+const signUp = createAction(SIGNUP, (result) => ({ result }));
+const login = createAction(LOGIN, (result) => ({ result }));
+const logOut = createAction(LOGOUT, (result) => ({ result }));
+const idCheck = createAction(IDCHECK, (result) => ({ result }));
+const nicknameCheck = createAction(NICKNAMECHECK, (result) => ({ result }));
+const myInfo = createAction(MYINFO, (myinfo) => ({ myinfo }));
+const userInfo = createAction(USERINFO, (userinfo) => ({ userinfo }));
+const editinfo = createAction(EDITMYINFO, (editinfo) => ({ editinfo }));
+
+export default handleActions({});
+
+const userAction = {};
+
+export { userAction };
