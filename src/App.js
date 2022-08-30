5cf98aec0175194a8ccc3ab957cea6b92b2ddc31
hongsoom <hongsoom@kakao.com>
2022-08-30 21:07:08 +0900

    Feat : feat css 초기화 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
+import GlobalStyles from "./components/share/GlobalStyles";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/main" element={<Main />} />
-    </Routes>
+    <div className="App">
+      <GlobalStyles />
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </div>
   );
 }
 
