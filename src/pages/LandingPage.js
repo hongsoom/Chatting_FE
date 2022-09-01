f939880aff247e78f74715d3b8b81e00383b9381
hongsoom <hongsoom@kakao.com>
2022-09-01 22:26:00 +0900

    Feat : feat login.js를 컴포넌트 단위로 나눔

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "../components/landingpage/Login";
 
 function LandingPage() {
-  return <Login />;
+  const [isClient, setIsClient] = useState(false);
+  const checkClient = () => {
+    setIsClient(!isClient);
+  };
+  return (
+    <>
+      {!isClient ? (
+        <div>
+          <Login checkClient={checkClient} />
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
+  );
 }
 
 export default LandingPage;
