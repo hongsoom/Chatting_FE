6e80f9804014208368696fc7e91349138ff75324
hongsoom <hongsoom@kakao.com>
2022-09-02 13:26:19 +0900

    Fix : fix 로그아웃 후 이동 경로 변경

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -22,54 +22,11 @@ const myInfo = createAction(MYINFO, (myinfo) => ({ myinfo }));
 const userInfo = createAction(USERINFO, (userinfo) => ({ userinfo }));
 const editinfo = createAction(EDITMYINFO, (editinfo) => ({ editinfo }));
 
-const signUpDB = (inputs) => {
-  return async function (dispatch) {
-    const introduction = null;
-    const userImgUrl = null;
-    try {
-      const response = await instance.post("api/users/register", {
-        inputs,
-        userImgUrl: userImgUrl,
-        introduction: introduction,
-      });
-      const status = response.status;
-      dispatch(signUp(status));
-      if (response.status === 200) {
-        window.location.assign("/login");
-      }
-    } catch (err) {}
-  };
-};
-
-const logInDB = (inputs) => {
-  return async function (dispatch) {
-    try {
-      const response = await instance.post("api/user/login", {
-        inputs,
-      });
-      if (response.status === 200) {
-        const token = response.headers.authorization;
-
-        localStorage.setItem("token", token);
-
-        const status = response.data.status;
-        dispatch(login(status));
-      }
-      if (localStorage.getItem("token")) {
-        window.location.assign("/");
-      }
-    } catch (err) {
-      const status = err.response.data.status;
-      dispatch(login(status));
-    }
-  };
-};
-
 const logOutDB = () => {
   return async function (dispatch) {
     localStorage.removeItem("token");
     dispatch(logOut());
-    window.location.assign("/login");
+    window.location.assign("/");
   };
 };
 
@@ -95,17 +52,6 @@ const myInfoDB = () => {
 
 export default handleActions(
   {
-    [SIGNUP]: (state, action) =>
-      produce(state, (draft) => {
-        draft.status = action.payload.result;
-      }),
-
-    [LOGIN]: (state, action) =>
-      produce(state, (draft) => {
-        draft.isLogin = true;
-        draft.status = action.payload.result;
-      }),
-
     [LOGOUT]: (state, action) =>
       produce(state, (draft) => {
         draft.isLogin = false;
@@ -120,8 +66,6 @@ export default handleActions(
 );
 
 const userActions = {
-  signUpDB,
-  logInDB,
   logOutDB,
   myInfoDB,
 };
