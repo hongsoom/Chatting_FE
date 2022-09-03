b1ac12426841388452f725f8a226888585f49f41
hongsoom <hongsoom@kakao.com>
2022-09-03 18:47:35 +0900

    Fix : fix 회원가입 형식이 다른데도 가입되는 오류 해결

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Main from "./pages/Main";
+import Signup from "./components/landingpage/Signup";
 import GlobalStyles from "./components/share/GlobalStyles";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <GlobalStyles />
       <Routes>
+        <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/main" element={<Main />} />
       </Routes>
