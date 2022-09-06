1134fadf73355c6e53d292ae0ef117b40765f3e7
hongsoom <hongsoom@kakao.com>
2022-09-06 11:55:26 +0900

    Fix : fix userInfo 자료 없다고 나오는 오류 해결

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -45,7 +45,7 @@ const userInfoDB = () => {
         }
       )
       .then((res) => {
-        const data = res.data.userList;
+        const data = res.data;
         dispatch(userInfo(data));
       })
       .catch((error) => {});
