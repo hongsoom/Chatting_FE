e55d9a5bdc51b7ef21e45d46621d966c449b27ae
hongsoom <hongsoom@kakao.com>
2022-09-13 16:18:37 +0900

    Fix : Fix 서버 주소 env에 넣기

--- src/redux/request.js
+++ src/redux/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://15.165.205.156:8080",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { Authorization: localStorage.getItem("token") },
 });
 
