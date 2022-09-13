7fd90d1c58e97ca90b7a5e62d8f2e735b9c79eb0
hongsoom <hongsoom@kakao.com>
2022-09-13 16:43:18 +0900

    Feat : feat 웹소켓 연결 시도

--- src/components/chat/ChatModal.js
+++ src/components/chat/ChatModal.js
@@ -1,6 +1,6 @@
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
-import React, { useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import ChatContent from "./ChatContent";
@@ -12,6 +12,39 @@ const ChatModal = ({ RoomOpen }) => {
 
   const webSocket = new SockJS(`${process.env.REACT_APP_API_URL}/ws-stomp`);
   const stomp = Stomp.over(webSocket);
+
+  const stompConnect = () => {
+    try {
+      //stomp.debug = null;
+      //웹소켓 연결시 stomp에서 자동으로 connect이 되었다는것을
+      //console에 보여주는데 그것을 감추기 위한 debug
+
+      stomp.connect(
+        {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          type: "TALK",
+        },
+        () => {
+          stomp.subscribe(
+            `서버주소`,
+            (data) => {
+              const newMessage = JSON.parse(data.body);
+              //데이터 파싱
+            },
+            { Authorization: `Bearer ${localStorage.getItem("token")}` }
+          );
+        }
+      );
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    stompConnect();
+
+    return () => {
+      // 언마운트 시 연결 해제
+    };
+  }, []);
 
   return (
     <ChatListContainer>
