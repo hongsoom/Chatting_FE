3873dcd07ef6460e645f8caa60a0df0fb10e6744
hongsoom <hongsoom@kakao.com>
2023-04-02 22:37:31 +0900

    [Feat] 채팅 방 메세지 input 컴포넌트 생성

--- src/components/chat/ChatInput.js
+++ src/components/chat/ChatInput.js
@@ -1,15 +1,16 @@
-import React from "react";
-import styled from "styled-components";
-import { Button, Input } from "../../elements";
-import { TbSend } from "react-icons/tb";
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from 'elements';
+import { Input } from 'elements/Input';
+import { TbSend } from 'react-icons/tb';
 
 const ChatInput = ({ SendMessage, message, setMessage }) => {
-  const handleMessage = (e) => {
+  const handleMessage = e => {
     setMessage(e.target.value);
   };
 
-  const SendMessageEnter = (e) => {
-    if (e.key === "Enter") {
+  const SendMessageEnter = e => {
+    if (e.key === 'Enter') {
       SendMessage();
     }
   };
@@ -17,32 +18,31 @@ const ChatInput = ({ SendMessage, message, setMessage }) => {
   return (
     <ChatInputWrap>
       <Input
-        S
-        width="870px"
-        height="60px"
-        padding="10px"
-        borderColor="rgb(175, 176, 179)"
+        width='870px'
+        height='60px'
+        padding='10px'
+        borderColor='rgb(175, 176, 179)'
         style={{
-          borderRadius: "15px",
+          borderRadius: '15px',
         }}
-        placeholder="메시지를 입력해주세요."
-        className="chatInput"
-        autocomplete="off"
+        placeholder='메시지를 입력해주세요.'
+        className='chatInput'
+        autocomplete='off'
         maxLength={150}
         onChange={handleMessage}
         value={message}
-        onKeyUp={(e) => SendMessageEnter(e)}
+        onKeyUp={e => SendMessageEnter(e)}
       ></Input>
       <Button
         S
-        width="80px"
-        height="50px"
-        padding="10px"
-        className="chatButton"
+        width='80px'
+        height='50px'
+        padding='10px'
+        className='chatButton'
         onClick={SendMessage}
         disabled={!message}
       >
-        <TbSend size="40" color="#ffc0cb" />
+        <TbSend size='40' color='#ffc0cb' />
       </Button>
     </ChatInputWrap>
   );
