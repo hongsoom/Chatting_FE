fd6a1b0f7bed0e65e0df63f2f684730bce257fd9
hongsoom <hongsoom@kakao.com>
2023-04-06 20:49:46 +0900

    [Fix] myinfo props 넘겨주기

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { userActions } from 'redux/modules/user';
 import * as L from 'styles/LayoutStlye';
 import { Category, Title, Link, RandomChatList } from 'components';
@@ -7,20 +7,21 @@ import { Category, Title, Link, RandomChatList } from 'components';
 const Chat = () => {
   const dispatch = useDispatch();
 
+  const myInfo = useSelector(state => state.user.myinfo);
+
   const resetClick = () => {
     dispatch(userActions.userInfoDB());
   };
 
   useEffect(() => {
     resetClick();
-    dispatch(userActions.myInfoDB());
-  }, []);
+  }, [dispatch]);
 
   return (
     <>
       <L.Layout>
         <Title title='채팅' />
-        <RandomChatList />
+        <RandomChatList myInfo={myInfo} />
         <Link onClick={resetClick} />
       </L.Layout>
       <Category />
