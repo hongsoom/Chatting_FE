a4f1939219fbd6402c40c5226eabeb0046bc873d
hongsoom <hongsoom@kakao.com>
2023-04-11 23:47:58 +0900

    [Feat] 채팅 알림 기능 추가

--- src/components/common/Category.js
+++ src/components/common/Category.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { AiOutlineHome, AiOutlineMessage, AiOutlineUser } from 'react-icons/ai';
 
 const Category = () => {
   const navigate = useNavigate();
   const location = useLocation();
+
+  const notification = useSelector(state => state.chat.notification);
 
   return (
     <CategoryWrap>
@@ -15,6 +18,7 @@ const Category = () => {
         onClick={() => navigate('/meassge')}
         location={location.pathname}
       />
+      {notification && <NewNoti />}
       <UserIcon id='/mypage' onClick={() => navigate('/mypage')} location={location.pathname} />
     </CategoryWrap>
   );
@@ -29,6 +33,17 @@ const CategoryWrap = styled.div`
   border-bottom: 1px solid #dcdcdc;
   border-right: 1px solid #dcdcdc;
   border-left: 1px solid #dcdcdc;
+`;
+
+const NewNoti = styled.div`
+  width: 14px;
+  height: 14px;
+  border-radius: 10px;
+  background: #ffb6c1;
+  position: absolute;
+  right: 60%;
+  bottom: 60%;
+  filter: blur(2px);
 `;
 
 const HomeIcon = styled(AiOutlineHome)`
